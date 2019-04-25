58498814ebb6e0101f42fb70bad86cad1eb55144
Christian Aashamar <christian.aashamar@arctic.com>
2019-04-25 13:44:41 +0200

    Added unit tests for findReposWithoutRemote

--- src/modules/gitStatus/findReposWithoutRemote.js
+++ src/modules/gitStatus/findReposWithoutRemote.js
@@ -1,27 +1,51 @@
 const { exec } = require("child_process");
-const async = require("async");
 
+/**
+ * Find which repositories in a list that does not have a remote URL
+ * @param {Array<object>} repositories The repositories to check
+ * @returns {Promise<Array<object>>} The outdated repositories, with name, path and changes
+ */
 const findReposWithoutRemote = async repositories => {
-  let reposWithoutRemote = [];
+  /**
+   * Check if the repository has a remote URL
+   * @param {object} repo The repo to check
+   * @returns {Promise<object | null} Returns the repo if it does NOT have a remote, else null
+   */
+  const findGitRemote = repo => {
+    return new Promise(async (resolve, reject) => {
+      // The command to execute to check for remote
+      const command = "git config --get remote.origin.url";
 
-  const findGitRemote = (repo, callback) => {
-    exec(
-      "git config --get remote.origin.url",
-      { cwd: repo.path },
-      (error, stdout, stderr) => {
+      // Execute the command
+      exec(command, { cwd: repo.path }, (error, stdout, stderr) => {
+        let repoWithoutRemote = null;
+
+        // If the output is empty, the repo does not have a remote url
         if (stdout === "") {
-          reposWithoutRemote.push({
+          repoWithoutRemote = {
             name: repo.name,
             path: repo.path
-          });
+          };
         }
-        callback();
-      }
-    );
+
+        // Resolve promise
+        resolve(repoWithoutRemote);
+      });
+    });
   };
 
   return new Promise((resolve, reject) => {
-    async.map(repositories, findGitRemote, () => {
+    Promise.all(repositories.map(repo => findGitRemote(repo))).then(result => {
+      let reposWithoutRemote = [];
+
+      // For all results from the promises, only push to reposWithoutRemote arry
+      // if the result is not null, ie if the repo does not have remote.
+      for (let i = 0; i < result.length; i++) {
+        if (result[i] !== null) {
+          reposWithoutRemote.push(result[i]);
+        }
+      }
+
       resolve(reposWithoutRemote);
     });
   });
---
+++ test/unit/src/modules/gitStatus/findReposWithoutRemote.test.js
@@ -0,0 +1,70 @@
+const {
+  findReposWithoutRemote
+} = require("../../../../../src/modules/gitStatus");
+const { exec } = require("child_process");
+
+jest.mock("child_process");
+
+beforeEach(() => {
+  // Reset mock so that mockResolvedValueOnce works correctly
+  exec.mockReset();
+});
+
+describe("findReposWithoutRemote", () => {
+  test("should return empty array if no repositories is given", () => {
+    const repositories = [];
+
+    exec.mockImplementation((cmd, opt, cb) => cb(null, "test", null));
+
+    return findReposWithoutRemote(repositories).then(result => {
+      expect(result).toEqual([]);
+      expect(exec.mock.calls.length).toBe(0);
+    });
+  });
+
+  test("should return empty array if none all repositories have remote URLs", () => {
+    const repositories = [
+      { name: "proj", path: "testDir/proj" },
+      { name: "proj2", path: "testDir/dir2/proj2" }
+    ];
+
+    exec
+      .mockImplementationOnce((cmd, opt, cb) =>
+        cb(null, "remote url one", null)
+      )
+      .mockImplementationOnce((cmd, opt, cb) =>
+        cb(null, "remote url two", null)
+      );
+
+    return findReposWithoutRemote(repositories).then(result => {
+      expect(result).toEqual([]);
+      expect(exec.mock.calls.length).toBe(2);
+    });
+  });
+
+  test("should return array with one element if one of the repositories does not have remote", () => {
+    const repositories = [
+      { name: "proj", path: "testDir/proj" },
+      { name: "proj2", path: "testDir/dir2/proj2" },
+      { name: "proj3", path: "testDir/dir2/proj3" }
+    ];
+
+    exec
+      .mockImplementationOnce((cmd, opt, cb) =>
+        cb(null, "remote url one", null)
+      )
+      .mockImplementationOnce((cmd, opt, cb) => cb(null, "", null))
+      .mockImplementationOnce((cmd, opt, cb) =>
+        cb(null, "remote url three", null)
+      );
+
+    const expectedResult = [{ name: "proj2", path: "testDir/dir2/proj2" }];
+
+    return findReposWithoutRemote(repositories).then(result => {
+      expect(result).toEqual(expectedResult);
+      expect(exec.mock.calls.length).toBe(3);
+    });
+  });
+
+  test("should ", () => {});
+});
